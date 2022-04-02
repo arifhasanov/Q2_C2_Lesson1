9a0ebbc31eb3dff939546a057bf3496432729d66
arifhasanov@yahoo.com <37578820+arifhasanov@users.noreply.github.com>
2022-04-03 00:06:20 +0200

    Done

---
+++ Lesson1/Post.cs
@@ -0,0 +1,23 @@
+﻿namespace Lesson1;
+public class Post
+{
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string? title { get; set; }
+    public string? body { get; set; }
+
+    public override string ToString()
+    {
+        string result = "";
+        result += userId.ToString();
+        result += "\n";
+        result += id.ToString();
+        result += "\n";
+        result += title;
+        result += "\n";
+        result += body;
+        result += "\n";
+        result += "\n";
+        return result;
+    }
+}
---
+++ Lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿string? summaryTextToWriteToFile = "";
+
+for (int i = 4; i <= 13; i++)
+{
+    Task<Post> task = GetPosts(i);
+    summaryTextToWriteToFile += task.Result.ToString();
+}
+
+static async Task<Post> GetPosts(int postId)
+{
+    HttpClient client = new HttpClient();
+    string responseBody = await client.GetStringAsync($"https://jsonplaceholder.typicode.com/posts/{postId}");
+    Post? post = JsonSerializer.Deserialize<Post>(responseBody);
+    return post!;
+}
+
+string fileName = "result.txt";
+File.WriteAllText(fileName, summaryTextToWriteToFile);
